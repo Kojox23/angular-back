f7e2df018f005687ffa4e9eac323173aa86aa0d5
Kojox23 <demaille.lucas@gmail.com>
2023-05-04 22:21:53 +0200

    test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose"); // (Étape 1) Import du DRM mongoose
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var muscleRouter = require("./routes/muscle");
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/muscles", muscleRouter);
